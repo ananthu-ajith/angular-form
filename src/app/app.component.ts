7d58eeaadb33214866f5a926cbe538e28196d974
Ananthu <ananthuajith6587@gmail.com>
2024-04-04 22:29:59 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'forms';
+  form ={
+    fullname:'',
+    username:'',
+    email:'',
+    address:'',
+    dob:'',
+    password:'',
+    phoneNumber:'',
+
+  };
+  onSubmit() :void {
+    console.log(this.form);
+  }
 }
